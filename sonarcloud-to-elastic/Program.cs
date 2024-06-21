c615005f9facf4cf4c26075eb43b3169f05da44f
Sofyan Ardianto <sofyanard@gmail.com>
2024-06-21 16:46:34 +0700

    add httpclient :cow:

--- sonarcloud-to-elastic/Program.cs
+++ sonarcloud-to-elastic/Program.cs
@@ -4,7 +4,9 @@
 
     internal class Program
     {
-        static void Main(string[] args)
+        static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
         {
             /*
             // create a logger factory
@@ -30,9 +32,32 @@
             logger.LogCritical("Critical message");
             */
 
+            // https://learn.microsoft.com/en-us/dotnet/core/extensions/logging
+
             using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
             ILogger logger = factory.CreateLogger("Program");
             logger.LogInformation("Hello World! Logging is {Description}.", "fun");
+
+
+
+            // https://learn.microsoft.com/en-us/dotnet/api/system.net.http.httpclient
+
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            try
+            {
+                using HttpResponseMessage response = await client.GetAsync("http://www.contoso.com/");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(uri);
+
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
 
             Console.WriteLine("Hello, World!");
         }
