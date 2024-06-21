9e62c2091a78169514f865b1bfe5685c75bad5a2
Sofyan Ardianto <sofyanard@gmail.com>
2024-06-21 16:31:01 +0700

    add logging :speak_no_evil:

--- sonarcloud-to-elastic/Program.cs
+++ sonarcloud-to-elastic/Program.cs
@@ -1,9 +1,39 @@
 ﻿namespace sonarcloud_to_elastic
 {
+    using Microsoft.Extensions.Logging;
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            /*
+            // create a logger factory
+            var loggerFactory = LoggerFactory.Create(
+                    builder => builder
+                            // add console as logging target
+                            .AddConsole()
+                            // add debug output as logging target
+                            .AddDebug()
+                            // set minimum level to log
+                            .SetMinimumLevel(LogLevel.Debug)
+            );
+
+            // create a logger
+            var logger = loggerFactory.CreateLogger<Program>();
+
+            // logging
+            logger.LogTrace("Trace message");
+            logger.LogDebug("Debug message");
+            logger.LogInformation("Info message");
+            logger.LogWarning("Warning message");
+            logger.LogError("Error message");
+            logger.LogCritical("Critical message");
+            */
+
+            using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+            ILogger logger = factory.CreateLogger("Program");
+            logger.LogInformation("Hello World! Logging is {Description}.", "fun");
+
             Console.WriteLine("Hello, World!");
         }
     }
